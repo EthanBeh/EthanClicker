02c347cd2bbf193f13042120194d0b0b6708eb07
unknown <BT_3W2_02@BKTECH.LOCAL>
2024-10-16 10:53:15 -0400

    added save btuton

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 //Ethan clicker
 document.getElementById("count-el").innerText = 0
+let savedArr = [];
 function increment() {
     document.getElementById("count-el").innerHTML = parseInt(document.getElementById("count-el").innerText) + 1;
     console.log(document.getElementById("count-el").innerHTML);
+}
+function save() {
+    document.getElementById("saved").innerHTML = document.getElementById("saved").innerHTML + document.getElementById("count-el").innerText + " - "
 }
